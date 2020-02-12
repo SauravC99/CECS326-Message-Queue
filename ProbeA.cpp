27573a94cccf678e192685713cb75d9b9011b8cc
Saurav <sauravc41@gmail.com>
2020-02-12 13:31:38 -0800

    Initial commit

---
+++ ProbeA.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int alpha = 997;
+int beta = 257;
+int rho = 251;
+
+int generateValue() {
+    int num;
+    num = rand();
+    return num;
+}
+
+void sendToHub(int num) {
+
+}
+
+bool waitForResponse() {
+
+}
