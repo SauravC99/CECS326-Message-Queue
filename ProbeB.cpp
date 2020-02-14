2ee4036bd906d928bf7e08ac5d8d29008dffe1db
stevenone11 <stevencenteno13@gmail.com>
2020-02-13 23:09:26 -0800

    added probes b and c

---
+++ ProbeB.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <chrono>
+
+using namespace std;
+
+int alpha = 997;
+int beta = 257;
+int rho = 251;
+
+int value;
+
+//need to change so that it returns a num divisible by its magic seed (beta)
+int generateValue() {
+    int num;
+    num = rand();
+    return num;
+}
+
+void sendToHub(int num) {
+
+}
+
+//change so that it will terminate after 10000 messages using force patch file
+bool terminate(int num) {
+    if (num < 50)
+        return true;
+    return false;
+}
+
+int main() {
+    while (!terminate(100)) {
+        //produce reading
+        value = generateValue();
+        sendToHub(value);
+        
+    }
+}
