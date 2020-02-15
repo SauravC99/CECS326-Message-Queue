521e42d79030dc81d6959ff831a58eea27ed9059
stevenone11 <stevencenteno13@gmail.com>
2020-02-14 16:14:49 -0800

    Updated some code to probe A and DataHub will continue later today

--- DataHub.cpp
+++ DataHub.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -10,6 +18,15 @@ int rho = 251;
 int getReading() {
 
 }
+
+//Purpose of the hub is to recieve data from the probes,
+//then to output which probe sent the message and the data
+//that came with it
+
+//recieves valid readings from the probes (valid reading is
+//dependent on what number the random integer is divisible by)
+ 
+//if alpha is probe A, if beta Probe B, if rho Probe C
 
 bool isValid(int num, int probe) {
     if (probe == 1) {
--- ProbeA.cpp
+++ ProbeA.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <stdlib.h>
 #include <chrono>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
 using namespace std;
 
 int alpha = 997;
-int beta = 257;
-int rho = 251;
+//int beta = 257; not needed only probe B
+//int rho = 251; not needed only probe C
 
 int value;
 
+
+/*
+*    generates the value divisible by alpha and returns it
+**/
 int generateValue() {
     int num;
-    num = rand();
+    bool generated = false;
+    while (!generated) {
+        num = rand();
+        if (num % alpha = 0)
+            generated = true;
+    }
     return num;
 }
 
